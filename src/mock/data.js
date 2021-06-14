c8dc9719ae887ad210b6e195d3942ba84efcccd0
Milind Yadav <yadavmilind08@gmail.com>
2021-06-14 17:43:34 +0530

    Updated mock data

--- src/mock/data.js
+++ src/mock/data.js
@@ -67,8 +67,3 @@ export const footerData = {
     },
   ],
 };
-
-// Github start/fork buttons
-export const githubButtons = {
-  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
-};
