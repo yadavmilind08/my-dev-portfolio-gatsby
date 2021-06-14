10dd4f49f9b007f1e6c7fb7180ae4c628e523199
Milind Yadav <yadavmilind08@gmail.com>
2021-06-14 13:19:08 +0530

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -129,7 +129,7 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
+      <title>First Gatsby Site</title>
       <h1 style={headingStyles}>
         Congratulations
         <br />
